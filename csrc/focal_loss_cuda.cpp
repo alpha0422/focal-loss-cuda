454d1187b5f98a8bbe164b8b298f3a02b5543a15
Wil Kong <alpha0422@gmail.com>
2021-03-31 20:34:58 +0800

    Revert trading memory for compute due to accuracy consideration.

--- csrc/focal_loss_cuda.cpp
+++ csrc/focal_loss_cuda.cpp
@@ -4,14 +4,20 @@
 
 // CUDA forward declarations
 
-std::vector<at::Tensor> focal_loss_forward_cuda(
-    const at::Tensor &cls_output, const at::Tensor &cls_targets_at_level,
-    const at::Tensor &num_positives_sum, const int64_t num_real_classes,
-    const float alpha, const float gamma, const float smoothing_factor);
+at::Tensor focal_loss_forward_cuda(const at::Tensor &cls_output,
+                                   const at::Tensor &cls_targets_at_level,
+                                   const at::Tensor &num_positives_sum,
+                                   const int64_t num_real_classes,
+                                   const float alpha, const float gamma,
+                                   const float smoothing_factor);
 
 at::Tensor focal_loss_backward_cuda(const at::Tensor &grad_output,
-                                    const at::Tensor &partial_grad,
-                                    const at::Tensor &num_positives_sum);
+                                    const at::Tensor &cls_output,
+                                    const at::Tensor &cls_targets_at_level,
+                                    const at::Tensor &num_positives_sum,
+                                    const int64_t num_real_classes,
+                                    const float alpha, const float gamma,
+                                    const float smoothing_factor);
 
 // C++ interface
 
@@ -22,10 +28,11 @@ at::Tensor focal_loss_backward_cuda(const at::Tensor &grad_output,
   CHECK_CUDA(x);                                                               \
   CHECK_CONTIGUOUS(x)
 
-std::vector<at::Tensor> focal_loss_forward(
-    const at::Tensor &cls_output, const at::Tensor &cls_targets_at_level,
-    const at::Tensor &num_positives_sum, const int64_t num_real_classes,
-    const float alpha, const float gamma, const float smoothing_factor) {
+at::Tensor focal_loss_forward(const at::Tensor &cls_output,
+                              const at::Tensor &cls_targets_at_level,
+                              const at::Tensor &num_positives_sum,
+                              const int64_t num_real_classes, const float alpha,
+                              const float gamma, const float smoothing_factor) {
   CHECK_INPUT(cls_output);
   CHECK_INPUT(cls_targets_at_level);
   CHECK_INPUT(num_positives_sum);
@@ -36,12 +43,20 @@ std::vector<at::Tensor> focal_loss_forward(
 }
 
 at::Tensor focal_loss_backward(const at::Tensor &grad_output,
-                               const at::Tensor &partial_grad,
-                               const at::Tensor &num_positives_sum) {
+                               const at::Tensor &cls_output,
+                               const at::Tensor &cls_targets_at_level,
+                               const at::Tensor &num_positives_sum,
+                               const int64_t num_real_classes,
+                               const float alpha, const float gamma,
+                               const float smoothing_factor) {
   CHECK_INPUT(grad_output);
-  CHECK_INPUT(partial_grad);
+  CHECK_INPUT(cls_output);
+  CHECK_INPUT(cls_targets_at_level);
+  CHECK_INPUT(num_positives_sum);
 
-  return focal_loss_backward_cuda(grad_output, partial_grad, num_positives_sum);
+  return focal_loss_backward_cuda(grad_output, cls_output, cls_targets_at_level,
+                                  num_positives_sum, num_real_classes, alpha,
+                                  gamma, smoothing_factor);
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
--- focal_loss/focal_loss.py
+++ focal_loss/focal_loss.py
@@ -6,23 +6,37 @@ class FocalLoss(torch.autograd.Function):
     @staticmethod
     def forward(ctx, cls_output, cls_targets_at_level, num_positives_sum,
                 num_real_classes, alpha, gamma, label_smoothing=0.0):
-        loss, partial_grad = focal_loss_cuda.forward(cls_output,
-                                                     cls_targets_at_level,
-                                                     num_positives_sum,
-                                                     num_real_classes,
-                                                     alpha, gamma,
-                                                     label_smoothing)
+        loss = focal_loss_cuda.forward(cls_output,
+                                       cls_targets_at_level,
+                                       num_positives_sum,
+                                       num_real_classes,
+                                       alpha, gamma,
+                                       label_smoothing)
 
-        ctx.save_for_backward(partial_grad, num_positives_sum)
+        ctx.save_for_backward(cls_output, cls_targets_at_level,
+                              num_positives_sum,
+                              torch.IntTensor([num_real_classes]),
+                              torch.FloatTensor([alpha]),
+                              torch.FloatTensor([gamma]),
+                              torch.FloatTensor([label_smoothing]))
+
         return loss
 
     @staticmethod
     def backward(ctx, grad_loss):
-        partial_grad, num_positives_sum = ctx.saved_tensors
+        cls_output, cls_targets_at_level, num_positives_sum, num_real_classes, \
+            alpha, gamma, label_smoothing = ctx.saved_tensors
+        num_real_classes = num_real_classes.item()
+        alpha = alpha.item()
+        gamma = gamma.item()
+        label_smoothing = label_smoothing.item()
 
-        # The backward kernel is actually in-place to save memory space,
-        # partial_grad and grad_input are the same tensor.
-        grad_input = focal_loss_cuda.backward(grad_loss, partial_grad,
-                                              num_positives_sum)
+        grad_input = focal_loss_cuda.backward(grad_loss,
+                                              cls_output,
+                                              cls_targets_at_level,
+                                              num_positives_sum,
+                                              num_real_classes,
+                                              alpha, gamma,
+                                              label_smoothing)
 
         return grad_input, None, None, None, None, None, None
